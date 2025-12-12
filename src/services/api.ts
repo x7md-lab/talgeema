86f5d190531fe8e93ff455f6a18595284dd2d02d
x7md <me@x7md.net>
2025-12-12 23:31:21 +0300

    fix api path

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 import type { MenuResponse } from '@/types/menu';
 
 export const fetchMenu = async (): Promise<MenuResponse> => {
-  const response = await axios.get<MenuResponse>('/menu_dump.json');
+  const response = await axios.get<MenuResponse>('./menu_dump.json');
   return response.data;
 };
