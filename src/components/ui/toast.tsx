0d1877a9d8259371b3ff38a365c4a096ce409fb3
x7md <me@x7md.net>
2025-12-14 15:58:32 +0300

    close toast

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -6,6 +6,7 @@ import {
 	Info,
 	Loader,
 	TriangleAlert,
+	X,
 } from "lucide-react"
 
 import { buttonVariants } from "@/components/ui/button"
@@ -40,23 +41,23 @@ function ToastList() {
 	return (
 		<Toast.Portal data-slot="toast-portal">
 			<Toast.Viewport
-				className="fixed top-auto right-4 bottom-4 mx-auto flex w-[calc(100%_-_2rem)] sm:right-8 sm:bottom-8 sm:w-[356px] z-[9999]"
+				className="fixed top-4 right-4 bottom-auto mx-auto flex w-[calc(100%_-_2rem)] sm:right-8 sm:top-8 sm:w-[356px] z-[9999]"
 				data-slot="toast-viewport"
 			>
 				{toasts.map((toast) => (
 					<Toast.Root
 						key={toast.id}
 						toast={toast}
-						swipeDirection={["right", "down"]}
+						swipeDirection={["right", "up"]}
 						data-slot="toast"
 						className={cn(
-							"bg-popover absolute right-0 bottom-0 left-auto z-[calc(1000-var(--toast-index))] mr-0 flex w-full items-center justify-between gap-1.5 rounded-lg border bg-clip-padding p-4 shadow-lg transition-all [transition-property:opacity,transform] duration-200 ease-out select-none",
-							"after:absolute after:bottom-full after:left-0 after:h-[calc(var(--gap)+1px)] after:w-full after:content-['']", // Fill the gap between toasts
-							"[transform:translateX(var(--toast-swipe-movement-x))_translateY(calc(var(--toast-swipe-movement-y)+calc(min(var(--toast-index),10)*-1*var(--gap))))_scale(calc(max(0,1-(var(--toast-index)*0.1))))]", // Initial position and scale
-							"data-[expanded]:[transform:translateX(var(--toast-swipe-movement-x))_translateY(calc(var(--toast-offset-y)*-1+calc(var(--toast-index)*var(--gap)*-1)+var(--toast-swipe-movement-y)))]", // Expanded state animation
-							"data-[ending-style]:opacity-0 data-[limited]:opacity-0 data-[starting-style]:[transform:translateY(150%)] data-[starting-style]:opacity-0 data-[ending-style]:[&:not([data-limited])]:[transform:translateY(150%)]",
+							"bg-popover absolute right-0 top-0 left-auto z-[calc(1000-var(--toast-index))] mr-0 flex w-full items-center justify-between gap-1.5 rounded-lg border bg-clip-padding p-4 shadow-lg transition-all [transition-property:opacity,transform] duration-200 ease-out select-none",
+							"after:absolute after:top-full after:left-0 after:h-[calc(var(--gap)+1px)] after:w-full after:content-['']", // Fill the gap between toasts
+							"[transform:translateX(var(--toast-swipe-movement-x))_translateY(calc(var(--toast-swipe-movement-y)+calc(min(var(--toast-index),10)*1*var(--gap))))_scale(calc(max(0,1-(var(--toast-index)*0.1))))]", // Initial position and scale
+							"data-[expanded]:[transform:translateX(var(--toast-swipe-movement-x))_translateY(calc(var(--toast-offset-y)*1+calc(var(--toast-index)*var(--gap)*1)+var(--toast-swipe-movement-y)))]", // Expanded state animation
+							"data-[ending-style]:opacity-0 data-[limited]:opacity-0 data-[starting-style]:[transform:translateY(-150%)] data-[starting-style]:opacity-0 data-[ending-style]:[&:not([data-limited])]:[transform:translateY(-150%)]",
 							"data-[ending-style]:data-[swipe-direction=right]:[transform:translateX(calc(var(--toast-swipe-movement-x)+150%))_translateY(var(--offset-y))] data-[expanded]:data-[ending-style]:data-[swipe-direction=right]:[transform:translateX(calc(var(--toast-swipe-movement-x)+150%))_translateY(var(--offset-y))]", // Right swipe animation
-							"data-[ending-style]:data-[swipe-direction=down]:[transform:translateY(calc(var(--toast-swipe-movement-y)+150%))] data-[expanded]:data-[ending-style]:data-[swipe-direction=down]:[transform:translateY(calc(var(--toast-swipe-movement-y)+150%))]", // Down swipe animation
+							"data-[ending-style]:data-[swipe-direction=up]:[transform:translateY(calc(var(--toast-swipe-movement-y)-150%))] data-[expanded]:data-[ending-style]:data-[swipe-direction=up]:[transform:translateY(calc(var(--toast-swipe-movement-y)-150%))]", // Up swipe animation
 							toast.type === "success" &&
 								"bg-success border-success-border text-success-foreground",
 							toast.type === "error" &&
@@ -69,7 +70,7 @@ function ToastList() {
 						style={{
 							["--gap" as string]: "0.8rem",
 							["--offset-y" as string]:
-								"calc(var(--toast-offset-y) * -1 + (var(--toast-index) * var(--gap) * -1) + var(--toast-swipe-movement-y))",
+								"calc(var(--toast-offset-y) * 1 + (var(--toast-index) * var(--gap) * 1) + var(--toast-swipe-movement-y))",
 						}}
 					>
 						<div className="flex items-center gap-2">
@@ -104,6 +105,9 @@ function ToastList() {
 								{toast.actionProps.children}
 							</Toast.Action>
 						)}
+						<Toast.Close className="text-muted-foreground hover:text-foreground">
+							<X className="size-4" />
+						</Toast.Close>
 					</Toast.Root>
 				))}
 			</Toast.Viewport>
